90ece16eb90559785bbcdc642a6a335498579247
Nathan Samano <nsv9982@saintjoe.edu>
2016-05-29 23:39:41 -0400

    adding own jade view

--- routes/index.js
+++ routes/index.js
@@ -26,7 +26,7 @@ module.exports = function(passport){
 
 	/* Handle Login POST */
 	router.post('/login', passport.authenticate('login', {
-		successRedirect: '/home',
+		successRedirect: '/dashboard', //'/home',
 		failureRedirect: '/',
 		failureFlash : true  
 	}));
@@ -48,6 +48,11 @@ module.exports = function(passport){
 		res.render('home', { user: req.user });
 		//res.sendFile('index.html', options);
 		//res.render('index.html', { user: req.user }, options);
+	});
+
+	/* GET Dashboard Page */
+	router.get('/dashboard', isAuthenticated, function(req, res) {
+		res.render('dashboard', { user: req.user });
 	});
 
 	/* Handle Logout */
