965dd352e3caac99aa2b095b7492c88c4bf754f3
Nathan Samano <nsv9982@saintjoe.edu>
2016-05-30 15:42:22 -0400

    routing home to dashboard

--- routes/index.js
+++ routes/index.js
@@ -52,7 +52,7 @@ module.exports = function(passport){
 	/* GET Home Page */
 	router.get('/home', isAuthenticated, function(req, res){
 		//res.render('home', { user: req.user });
-		res.render('/dashboard', { user: req.user });
+		res.render('dashboard', { user: req.user });
 		//res.sendFile('index.html', options);
 		//res.render('index.html', { user: req.user }, options);
 	});
