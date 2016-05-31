0f3ae313311452a9d9971db72f31e70327fa9a8c
Nathan Samano <nsv9982@saintjoe.edu>
2016-05-31 18:30:59 -0400

    score set to null

--- passport/signup.js
+++ passport/signup.js
@@ -30,7 +30,7 @@ module.exports = function(passport){
                         newUser.username = username;
                         newUser.password = createHash(password);
                         newUser.displayName = req.param('displayName');
-                        newUser.score = req.param('score');
+                        newUser.score = null;
 
                         // save the user
                         newUser.save(function(err) {
