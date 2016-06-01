bd1a09d247d02247e140f851565cae004ff51c44
Nathan Samano <nsv9982@saintjoe.edu>
2016-05-31 20:09:50 -0400

    testing require on spa

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var dashboard = require('../public/js/spa.dashboard.js');
+var dashboard = require('../public/js/spa');
 
 var isAuthenticated = function (req, res, next) {
 	// if user is authenticated in the session, call the next() to call the next request handler 
